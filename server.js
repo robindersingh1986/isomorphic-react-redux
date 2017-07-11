a5507f114f87752d4563636a7de78105b99509e2
Robinder Singh <robindersingh1986@gmail.com>
2017-07-12 00:00:16 +0530

    moving to isomorphic render part, partial update

--- server.js
+++ server.js
@@ -2,12 +2,51 @@ require("babel-register")({
 	presets:["react"]
 });
 
-import express from "express";
-let app = express();
+/*import express from "express";
+import React from "react";  
+import Router from "react-router"; 
+import { Route } from "react-router";  
+
+*/
+
+import express                   from 'express';
+import React                     from 'react';
+import { renderToString }        from 'react-dom/server'
+import { RoutingContext, match } from 'react-router';
+import createLocation            from 'history/lib/createLocation';
+import routes                    from './routes';
+import { Provider }              from 'react-redux';
+import * as reducers             from 'reducers';
+import promiseMiddleware         from 'lib/promiseMiddleware';
+import fetchComponentData        from 'lib/fetchComponentData';
+import { createStore,
+         combineReducers,
+         applyMiddleware }       from 'redux';
+import path                      from 'path';
+
+
+
+const app = express();
 app.use(express.static("public"));
-app.use(require("./routes/index.jsx"));
+//app.use(require("./routes/index.jsx"));
+
+var App = require("./Components/App.jsx");
 
 
+app.get("*", function(request, response){
+
+	Router.run( <Route handler={ App } path="/" />, request.url, App => {
+    
+	var html = ReactDOMServer.renderToString(
+	 		React.createElement(App)
+	 	);
+	 response.send(html);
+    /*let content = React.renderToString(<Handler />);
+    res.render('index', { content: content });*/
+  });
+
+	console.log("Server side rendering , route url called : ", request.url);
+});
 
 /*var React = require("react");
 var ReactDOMServer = require("react-dom/server");
