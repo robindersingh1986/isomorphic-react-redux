4b1b2b6e5567e51e7f37cf6506fb70a7958496b1
Robinder Singh <robindersingh1986@gmail.com>
2017-07-13 21:36:33 +0530

    added comment

--- App/actions/todoAction.js
+++ App/actions/todoAction.js
@@ -1,6 +1,6 @@
 import request from 'axios';
 const API_URL = 'https://webtask.it.auth0.com/api/run/wt-milomord-gmail_com-0/redux-tutorial-backend?webtask_no_cache=1';
-
+//call api here
 
 
 export const todoAct = (todo) => {
